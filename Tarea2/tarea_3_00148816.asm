15f963770e6fc21512e015370dff23188e262901
vegadevh <00148816@uca.edu.sv>
2020-04-26 11:48:10 -0600

    Error, copia de archivo actualizada

--- Tarea2/tarea_3_00148816.asm
+++ Tarea2/tarea_3_00148816.asm
@@ -4,27 +4,14 @@
 	org	100h
 
 	mov	ax, 0000h
-	mov	bx, 0000h
-	mov	bx, 220h
 
 	mov	ax, 0d
-	mov	[bx], ax
-	mov	bx, 221h
+	mov	[220h], ax
 
 	mov	ax, 1d
-	mov	[bx], ax
+	mov	[221h], ax
 
 fib:	mov	ax, 000h ;limpiando ax
-	mov	[bx], ax
-	sub	bx, 2h ;La resta es almacenada en bx
-	mov	al, [bx]
-	add	bx, 1h ;suma almacenada en bx
-	add	ax, [bx] ; valor de bx almacenado en ax
-	add	bx, 1h ; 1h para bx
-	mov	[bx], ax
-	add	bx, 1h
 
-	cmp	bx, 22Fh
-	jb	fib ;Al cumplir la comparacion si bx es menor salta realiza fibo
 
 	int	20h
