134bb5f7f93866683d051e5d0546f2a0222fa89b
vegadevh <00148816@uca.edu.sv>
2020-04-26 13:26:25 -0600

    Agrega segunda parte, funcionalidad de ejercicio

--- Tarea2/tarea_3_00148816.asm
+++ Tarea2/tarea_3_00148816.asm
@@ -10,8 +10,19 @@
 
 	mov	ax, 1d
 	mov	[221h], ax
+	mov	bx, 222h
 
 fib:	mov	ax, 000h ;limpiando ax
+	mov	[bx], ax
+	sub	bx, 2h ;La resta es almacenada en bx
+	mov	al, [bx]
+	add	bx, 1h ;suma almacenada en bx
+	add	ax, [bx] ; valor de bx almacenado en ax
+	add	bx, 1h ; 1h para bx
+	mov	[bx], ax
+	add	bx, 1h
 
+	cmp	bx, 22Fh
+	jb	fib ;Al cumplir la comparacion si bx es menor salta realiza fibo
 
 	int	20h
