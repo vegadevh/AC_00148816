4624d2766013d58e0dd6b5b69d598d3982f12a1c
vegadevh <00148816@uca.edu.sv>
2020-05-20 14:24:29 -0600

    Agregando mensajes problema con mensajes

--- Tarea3/tarea.asm
+++ Tarea3/tarea.asm
@@ -5,6 +5,8 @@ section .text
 	call 	texto	
 	call 	cursor
 	call 	phrase
+	call	phrase2
+	call	phrase3
 	call	kbwait
 
 	int 	20h
@@ -34,10 +36,15 @@ kbwait: mov 	ax, 0000h
 
 m_cursr:mov 	ah, 02h
 	mov 	dx, di  ; columna
-	add	dl, 29d
 	mov 	dh, 12d ; fila
 	mov 	bh, 0h
 	int 	10h
+	ret
+
+saltos:	mov	[200h], ax
+	mov	ax, 3h
+	add	[203h],ax
+	mov	ax, [200h]
 	ret
 
 phrase:	mov 	di, 0d
@@ -47,9 +54,34 @@ lupi:	mov 	cl, [msg+di]
 	inc	di
 	cmp 	di, len
 	jb	lupi
+	call	saltos
+	ret
+
+phrase2:	mov 	di, 0d
+lupi2:	mov 	cl, [msg2+di]
+	call    m_cursr
+	call 	w_char
+	inc	di
+	cmp 	di, len
+	jb	lupi
+	call	saltos
+	ret
+
+phrase3:	mov 	di, 0d
+lupi3:	mov 	cl, [msg3+di]
+	call    m_cursr
+	call 	w_char
+	inc	di
+	cmp 	di, len
+	jb	lupi
+	call	saltos
 	ret
 
 
 section .data
-msg	db 	"Mami que tu quiere? "
+msg	db 	"Muy wenas? "
 len 	equ	$-msg
+msg2	db	"Como tas?"
+len2 	equ	$-msg2
+msg3	db	"Saludos a tu mami"
+len3 	equ	$-msg3
